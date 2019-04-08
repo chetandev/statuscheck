813d703a4309412aa1679caeea741b2b8ab853a8
Rahul-Aanand <rahul.anand.eee12@itbhu.ac.in>
2019-04-08 16:38:13 +0530

    Update longrunning.sh

--- longrunning.sh
+++ longrunning.sh
@@ -6,5 +6,5 @@
 #echo vmstat,tag=1 r=$r,swpd=$swpd,rh=1
 
 count=0 
-count="$(ps -eo pid,comm,pcpu,etimes| awk '{if($3>=0.1 && $4>3000){printf "%s %6s \n",  $2,$4}}' | wc -l)"
+count="$(ps -eo pid,comm,pcpu,etimes| awk '{if($3>=0.6 && $4>300){printf "%s %6s \n",  $2,$4}}' | wc -l)"
 echo longrunning,tag=1 lrn=$count,rcb=1
