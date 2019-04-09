398fa3c52f89ca67fb58942a33da9add205fa632
Rahul-Aanand <rahul.anand.eee12@itbhu.ac.in>
2019-04-09 17:39:35 +0530

    adding queued processes

--- longrunning.sh
+++ longrunning.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #r=0
 #swpd=0
-#r="$(vmstat | awk '(NR==2){for(i=1;i<=NF;i++)if($i=="r"){getline; print $i}}')"
+r="$(vmstat | awk '(NR==2){for(i=1;i<=NF;i++)if($i=="r"){getline; print $i}}')"
 #swpd="$(vmstat | awk '(NR==2){for(i=1;i<=NF;i++)if($i=="swpd"){getline; print $i}}')"
 #echo vmstat,tag=1 r=$r,swpd=$swpd,rh=1
 
 count=0 
 count="$(ps -eo pid,comm,pcpu,etimes| awk '{if($3>=0.6 && $4>300){printf "%s %6s \n",  $2,$4}}' | wc -l)"
 echo longrunning,tag=1 lrn=$count,rcb=1
+echo runqueue,tag=1 queued=$r
