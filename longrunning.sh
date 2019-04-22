9f8944def61ede09142034116e7ca58e1944e6f3
Rahul-Aanand <rahul.anand.eee12@itbhu.ac.in>
2019-04-22 16:21:48 +0530

    enabling run queue in test branch

--- longrunning.sh
+++ longrunning.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #r=0
 #swpd=0
-#r="$(vmstat | awk '(NR==2){for(i=1;i<=NF;i++)if($i=="r"){getline; print $i}}')"
+r="$(vmstat | awk '(NR==2){for(i=1;i<=NF;i++)if($i=="r"){getline; print $i}}')"
 #swpd="$(vmstat | awk '(NR==2){for(i=1;i<=NF;i++)if($i=="swpd"){getline; print $i}}')"
 #echo vmstat,tag=1 r=$r,swpd=$swpd,rh=1
 
 count=0 
 count="$(ps -eo pid,comm,pcpu,etimes| awk '{if($3>=0.1 && $4>3000){printf "%s %6s \n",  $2,$4}}' | wc -l)"
-echo longrunning,tag=1 lrn=$count,rcb=1
+echo longrunning,tag=1 lrn=$count,rcb=1,runqueue=$r
