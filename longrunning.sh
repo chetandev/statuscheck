faf4ebf8a655a36db1336fc6dfdbd7b9f0f3e138
Rahul-Aanand <rahul.anand.eee12@itbhu.ac.in>
2019-04-23 12:31:52 +0530

    changing long running to 60 percent and 300 secs

--- longrunning.sh
+++ longrunning.sh
@@ -6,5 +6,5 @@ r="$(vmstat | awk '(NR==2){for(i=1;i<=NF;i++)if($i=="r"){getline; print $i}}')"
 #echo vmstat,tag=1 r=$r,swpd=$swpd,rh=1
 
 count=0 
-count="$(ps -eo pid,comm,pcpu,etimes| awk '{if($3>=0.1 && $4>3000){printf "%s %6s \n",  $2,$4}}' | wc -l)"
+count="$(ps -eo pid,comm,pcpu,etimes| awk '{if($3>=0.6 && $4>300){printf "%s %6s \n",  $2,$4}}' | wc -l)"
 echo longrunning,tag=1 lrn=$count,rcb=1,runqueue=$r
