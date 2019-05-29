d923e0cbcab942f9a068863f702a30862b0b18dc
Rahul Anand <rahul5.anand@ril.com>
2019-05-29 11:39:33 +0530

    changing tail to include missing commands

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 telgu="$(id -u telegraf )"
-log="$(tail -n 10 /var/log/auth.log | grep --line-buffered snoopy | grep -v $telgu)"
+log="$(tail -n 15 /var/log/auth.log | grep --line-buffered snoopy | grep -v $telgu)"
 base="$(echo  $log |openssl base64)"
 echo commands,tag=commands log="\"${base}\""
 
